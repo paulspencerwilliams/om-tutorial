2075de69fe9aacc5f18865b6fe2a5dd3c9165400
Paul Williams <paul@paulswilliams.me.uk>
2014-10-14 19:39:19 +0100

    renamed atom keys to reflect tasks

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -11,22 +11,13 @@
 (def app-state
   (atom
     {:tasks
-     [{:first "Ben" :last "Bitdiddle"}
-      {:first "Alyssa" :middle-initial "P" :last "Hacker"}
-      {:first "Eva" :middle "Lu" :last "Ator"}
-      {:first "Louis" :last "Reasoner"}
-      {:first "Cy" :middle-initial "D" :last "Effect"}
-      {:first "Lem" :middle-initial "E" :last "Tweakit"}]}))
+     [{:title "Organise BCS night" :due "2014-11-04"}
+      {:title "Learn pedestal" :due "2014-12-31"}
+      {:title "Learn Om" :due "2014-11-03"}]}))
 
 (defn parse-task [task-str]
-  (let [[first middle last :as parts] (string/split task-str #"\s+")
-        [first last middle] (if (nil? last) [first middle] [first last middle])
-        middle (when middle (string/replace middle "." ""))
-        c (if middle (count middle) 0)]
-    (when (>= (count parts) 2)
-      (cond-> {:first first :last last}
-        (== c 1) (assoc :middle-initial middle)
-        (>= c 2) (assoc :middle middle)))))
+  (let [[title due :as parts] (string/split task-str #"\s+")]
+    {:title title :due due}))
 
 (defn add-task [app owner]
   (let [new-task (-> (om/get-node owner "new-task")
@@ -35,8 +26,8 @@
     (when new-task
       (om/transact! app :tasks #(conj % new-task)))))
 
-(defn display-name [{:keys [first last] :as task}]
-  (str first ", " last))
+(defn display-name [{:keys [title due] :as task}]
+  (str title " due by " due))
 
 (defn task-view [task owner]
   (reify
