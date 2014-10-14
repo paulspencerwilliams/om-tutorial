bb6f101c5f2010dbfb28a6ffe0dc7ae49a024f8f
Paul Williams <paul@paulswilliams.me.uk>
2014-10-14 17:46:36 +0100

    outputing contacts

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -4,16 +4,38 @@
 
 (enable-console-print!)
 
-(def app-state (atom {:list ["Lion" "Zebra" "Buffalo" "Antelope"]}))
+(def app-state
+  (atom
+    {:contacts
+     [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+      {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
+      {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+      {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+      {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
+      {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]}))
 
-(defn stripe [text bgc]
-  (let [st #js {:backgroundColor bgc}]
-    (dom/li #js {:style st} text)))
+(defn middle-name [{:keys [middle middle-initial]}]
+  (cond
+    middle (str " " middle)
+    middle-initial (str " " middle-initial ".")))
 
-(om/root
-  (fn [app owner]
-    (om/component
-      (apply dom/ul #js {:className "animals"}
-        (map stripe (:list app) (cycle ["#ff0" "#fff"])))))
-  app-state
-  {:target (. js/document (getElementById "app0"))})
+(defn display-name [{:keys [first last] :as contact}]
+  (str last ", " first (middle-name contact)))
+
+(defn contact-view [contact owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/li nil (display-name contact)))))
+
+(defn contacts-view [app owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div nil
+        (dom/h2 nil "Contact list")
+        (apply dom/ul nil
+          (om/build-all contact-view (:contacts app)))))))
+
+(om/root contacts-view app-state
+  {:target (. js/document (getElementById "contacts"))})
